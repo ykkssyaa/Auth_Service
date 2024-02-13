8255d50dc717c4090a3f2b55ede06bf92b7b2da9
ykkssyaa <mr.ender.03@mail.ru>
2024-02-13 19:02:02 +0300

    add config

--- cmd/app.go
+++ cmd/app.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"AuthService/config"
+	lg "AuthService/pkg/logger"
+	"github.com/spf13/viper"
+)
+
 func main() {
+
+	logger := lg.InitLogger()
+
+	logger.Info.Print("Executing InitConfig.")
+	if err := config.InitConfig(); err != nil {
+		logger.Err.Fatalf(err.Error())
+	}
+
+	logger.Info.Println("SERVER PORT: " + viper.GetString("port"))
+
+	logger.Info.Println("Connecting to MongoDB.")
 
 }
---
+++ config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import "github.com/spf13/viper"
+
+func InitConfig() error {
+
+	viper.SetConfigFile("config/.env")
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		return err
+	}
+
+	viper.SetConfigFile("config/config.yml")
+	err = viper.ReadInConfig()
+
+	return err
+}
